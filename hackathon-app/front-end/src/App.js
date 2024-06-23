3917e508d6d78c6493f21469228157ffd63244af
Felixtrp <64906690+Felixtrp@users.noreply.github.com>
2024-06-23 15:29:27 +0100

    added pages

--- hackathon-app/front-end/src/App.js
+++ hackathon-app/front-end/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Questions from './components/Questions';
 import CaseRepo from './components/CaseRepo';
+import Form from './components/Form';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/questions" element={<Questions />} />
         <Route path="/CaseRepo" element={<CaseRepo />} />
+        <Route path="/Form" element={<Form />} />
       </Routes>
     </Router>
   );
