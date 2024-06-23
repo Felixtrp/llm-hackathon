38fd8072782988cee8ed253f4735e9409eebcce2
Felixtrp <64906690+Felixtrp@users.noreply.github.com>
2024-06-23 14:19:56 +0100

    Added some new pages - "Your case" form and "case repo"

--- hackathon-app/front-end/src/App.js
+++ hackathon-app/front-end/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Questions from './components/Questions';
+import CaseRepo from './components/CaseRepo';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/questions" element={<Questions />} />
+        <Route path="/CaseRepo" element={<CaseRepo />} />
       </Routes>
     </Router>
   );
---
+++ hackathon-app/front-end/src/components/CaseRepo.js
@@ -0,0 +1,20 @@
+function CaseRepo() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '10%' }}>
+        <h1>Case Repository</h1>
+        <form>
+          <div style={{ marginBottom: '20px' }}>
+            <h2>Here are the current open cases:</h2>
+          </div>
+          <div style={{ marginBottom: '20px' }}>
+            <h3>Case 1</h3>
+            <h3>Case 2</h3>
+            <h3>Case 3</h3>
+          </div>
+        </form>
+      </div>
+    );
+  }
+  
+  export default CaseRepo;
+  
